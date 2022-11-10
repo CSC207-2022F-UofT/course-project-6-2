e43955b820ad1eb689f349760bc2a0e879f25941
lisacheng12 <jiayilisa27@gmail.com>
2022-11-10 17:19:03 -0500

    issue 1 update

--- src/main/java/users/User.java
+++ src/main/java/users/User.java
@@ -13,7 +13,35 @@ public abstract class User {
         this.address = address;
     }
 
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public String getPhoneNumber() {
         return phoneNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPassWord() {
+        return passWord;
     }
 }
