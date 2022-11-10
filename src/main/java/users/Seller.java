5bf5b569039dd052869e01920e7d78355807388b
lisacheng12 <jiayilisa27@gmail.com>
2022-11-10 17:27:53 -0500

    new docstings added for seller class.

--- src/main/java/users/Seller.java
+++ src/main/java/users/Seller.java
@@ -8,6 +8,16 @@ public class Seller extends User {
     public ArrayList<String> orderInProgress;
     public ArrayList<String> orderCompleted;
 
+    /**
+     * Construct an instance of the entity Seller.
+     * @param accountName The account name of the seller.
+     * @param phoneNumber The phone number of the seller.
+     * @param passWord The password of the seller.
+     * @param address The address of the seller.
+     * @param storeName The store name.
+     * @param orderInProgress The list of order numbers that are in progress.
+     * @param orderCompleted The list of order numbers that are already completed.
+     */
     public Seller(String accountName, String phoneNumber, String passWord, String address, String storeName, ArrayList<String> orderInProgress, ArrayList<String> orderCompleted) {
         super(accountName, phoneNumber, passWord, address);
         this.storeName = storeName;
