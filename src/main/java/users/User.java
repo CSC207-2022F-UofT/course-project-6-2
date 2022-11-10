f5805e674f742cb27aa9010b79ea4715cd65ae2a
donnaqi <donna021107@gmail.com>
2022-11-10 17:24:23 -0500

    Revert "issue 1 update"
    This reverts commit 8de4c8c3211031716998bcf020eeb5d6565bd584.

--- src/main/java/users/User.java
+++ src/main/java/users/User.java
@@ -1,49 +1,4 @@
 package users;
 
 public class User {
-    public String name;
-    public String password;
-    public int phoneNumber;
-
-    /**
-     * Construct an instance of User
-     * @param name The nickname of the user.
-     * @param password The password of the user.
-     * @param phoneNumber The phone number of the user.
-     */
-    public User(String name, String password, int phoneNumber){
-        this.name = name;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-    }
-
-    /**
-     * Getter methods for the three instance value.
-     */
-    public String getName(){
-        return this.name;
-    }
-
-    public String getPassword(){
-        return this.password;
-    }
-
-    public int getPhoneNumber(){
-        return this.phoneNumber;
-    }
-
-    /**
-     * Setter methods for the instance values.
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
