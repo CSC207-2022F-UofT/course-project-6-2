008013e5522b9572937aa075c7671f33f2636ebd
lisacheng12 <jiayilisa27@gmail.com>
2022-11-11 00:18:14 -0500

    add more methods for Drink class

--- src/main/java/drink/Drink.java
+++ src/main/java/drink/Drink.java
@@ -12,6 +12,17 @@ public class Drink {
     private Date expirationDate;
     private float discount;
 
+    /**
+     * Construct an instance of the entity drink.
+     * @param name The name of the drink.
+     * @param price The price of the drink.
+     * @param description The brief description of the drink.
+     * @param ingredient The ingredient of the drink.
+     * @param volume The volume in ml of the drink.
+     * @param productionData The date of production of the drink.
+     * @param expirationDate The date of expiration of the drink.
+     * @param discount The amount of discount of this drink right now.
+     */
     public Drink(String name, float price, String description, String ingredient, int volume, Date productionData, Date expirationDate, float discount) {
         this.name = name;
         this.price = price;
@@ -21,5 +32,76 @@ public class Drink {
         this.productionData = productionData;
         this.expirationDate = expirationDate;
         this.discount = discount;
+    }
+
+    /**
+     * The setter methods of the instance values.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setProductionData(Date productionData) {
+        this.productionData = productionData;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+
+
+    /**
+     * The getter methods of the instance values.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getIngredient(){
+        return this.ingredient;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+    public Date getProductionData() {
+        return this.productionData;
+    }
+
+    public Date getExpirationDate() {
+        return this.expirationDate;
+    }
+
+    public float getDiscount() {
+        return this.discount;
     }
 }
