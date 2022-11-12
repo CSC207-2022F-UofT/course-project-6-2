982ed4464ca584df1efda7c63789bec826562c78
sanciag17 <sancia.gao@mail.utoronto.ca>
2022-11-12 18:24:57 -0500

    Start interface for order history, add UI package, add OrderHistoryView Class,add UI controller class

---
+++ src/main/java/Controller/UIController.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UIController implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+}
