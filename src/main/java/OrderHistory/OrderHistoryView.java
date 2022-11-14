13bb8f08e3bfda415d5063b6ca22f65e6e5acea6
Sancia <sancia.gao@mail.utoronto.ca>
2022-11-14 11:15:34 +0800

    Revert "Start interface for order history, add UI package, add OrderHistoryView Class,add UI controller class"
    This reverts commit 982ed4464ca584df1efda7c63789bec826562c78.

--- src/main/java/OrderHistory/OrderHistoryView.java
+++ src/main/java/OrderHistory/OrderHistoryView.java
@@ -1,40 +1,48 @@
 package OrderHistory;
 
-import Controller.UIController;
-
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class OrderHistoryView implements ActionListener {
 
-public class OrderHistoryView{
+    static JFrame frame = new JFrame();
+    static JPanel panel = new JPanel();
+    static JTextField orderNumber = new JTextField();
+    static JButton search = new JButton("Search");
+    static JButton nextPage = new JButton("Next Page");
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Order History");
         frame.add(panel);
+        frame.setLayout(null);
+        frame.setVisible(true);
         panel.setLayout(null);
 
-        JTextField orderNumber = new JTextField();
         orderNumber.setBounds(25, 25, 225, 30);
         frame.add(orderNumber);
 
-        JButton search = new JButton("Search");
         search.setBounds(275,25,75,30);
         frame.add(search);
 
-        JButton nextPage = new JButton("Next Page");
         nextPage.setBounds(500,500,125,30);
         frame.add(nextPage);
 
         JLabel order1 = new JLabel("Order 1");
         order1.setBounds(45, 75, 80,20);
         frame.add(order1);
-
-        frame.setLayout(null);
-        frame.setVisible(true);
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String s = e.getActionCommand();
+
+        if(s.equals("Next Page")) {
+            frame.getContentPane().setBackground(Color.pink);
+            frame.setVisible(true);
+        }
+    }
 }
