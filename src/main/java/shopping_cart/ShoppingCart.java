d20acab37e1b46377078377cc3cecdcac9a8350d
lisacheng12 <jiayilisa27@gmail.com>
2022-11-16 05:16:29 -0500

    create the shopping cart class and adding the generate order number method

--- src/main/java/shopping_cart/ShoppingCart.java
+++ src/main/java/shopping_cart/ShoppingCart.java
@@ -1,4 +1,39 @@
 package shopping_cart;
 
+import drink.Drink;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ShoppingCart {
+    public float totalPrize;
+    public HashMap<Drink, String> itemList;
+    public ShoppingCart(float totalPrize, HashMap<Drink, String> itemList){
+        this.totalPrize = totalPrize;
+        this.itemList = itemList;
+    }
+
+    /**
+     * The setter methods of the instance values.
+     */
+    public void setTotalPrize(float totalPrize) {
+        this.totalPrize = totalPrize;
+    }
+
+    public void setItemList(HashMap<Drink, String> itemList) {
+        this.itemList = itemList;
+    }
+
+    /**
+     * The getter methods of the instance values.
+     */
+    public float getTotalPrize() {
+        return this.totalPrize;
+    }
+
+    public HashMap<Drink, String> getItemList() {
+        return this.itemList;
+    }
+
+
 }
