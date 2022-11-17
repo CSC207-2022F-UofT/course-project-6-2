cc40cc6fed7864e39b7d63f30e8f31fb6f095cdb
lisacheng12 <jiayilisa27@gmail.com>
2022-11-17 03:41:12 -0500

    fixed the generate order number method to make every order number unique

--- src/main/java/order/Order.java
+++ src/main/java/order/Order.java
@@ -10,28 +10,32 @@ public class Order {
     public ArrayList<String> orderQuantities;
     public HashMap<String, String> orderList;
     public String orderStatus;
+    public static HashMap<String, Order> totalOrder;
 
     /**
      * Construct an instance of the Entity Order.
-     * @param orderNum The order number of a single order with len 10.
-     * @param orderItems The item names in this order.
+     *
+     * @param orderNum        The order number of a single order with len 10.
+     * @param orderItems      The item names in this order.
      * @param orderQuantities The quantities of each item that customer has order.
-     * @param orderList The list that has all the items and the corresponding quantities for this order.
-     * @param orderStatus A string indicating the current status of the order, including [inProgress, ready, pickedUp, delivered].
+     * @param orderList       The list that has all the items and the corresponding quantities for this order.
+     * @param orderStatus     A string indicating the current status of the order, including [inProgress, ready, pickedUp, delivered].
+     * @param totalOrder      A hashmap that keep track of all the orders.
      */
-    public Order(String orderNum, ArrayList<String> orderItems, ArrayList<String> orderQuantities, HashMap<String, String> orderList, String orderStatus){
+    public Order(String orderNum, ArrayList<String> orderItems, ArrayList<String> orderQuantities, HashMap<String, String> orderList, String orderStatus, HashMap<String, Order> totalOrder) {
         this.orderNum = orderNum;
         this.orderItems = orderItems;
         this.orderQuantities = orderQuantities;
         this.orderList = orderList;
         this.orderStatus = orderStatus;
+        this.totalOrder = totalOrder;
     }
 
     /**
      * The setter method of the instance values.
      */
     public void setOrderNum(String orderNum) {
-        this.orderNum = orderNum;
+        this.orderNum = generateOrderNum();
     }
 
     public void setOrderItems(ArrayList<String> orderItems) {
@@ -43,13 +47,17 @@ public class Order {
     }
 
     public void setOrderList(ArrayList<String> orderItems, ArrayList<String> orderQuantities) {
-        for (int i = 0; i < getOrderItems().size(); i++){
+        for (int i = 0; i < getOrderItems().size(); i++) {
             this.orderList.put(orderItems.get(i), orderQuantities.get(i));
         }
     }
 
     public void setOrderStatus(String orderStatus) {
         this.orderStatus = orderStatus;
+    }
+
+    public static void setTotalOrder(HashMap<String, Order> totalOrder) {
+        Order.totalOrder = totalOrder;
     }
 
     /**
@@ -75,13 +83,22 @@ public class Order {
         return this.orderStatus;
     }
 
-    /**
-     * Generate a random string represent as the order number of the order.
-     * @return A random generated string with digits and letters.
-     */
-    public static String generateOrderNum(){
-        UUID randomUUID = UUID.randomUUID();
-        return randomUUID.toString().replaceAll("-", "").substring(0, 10);
+    public static HashMap<String, Order> getTotalOrder() {
+        return totalOrder;
     }
 
+    /**
+     * Generate a random string represent as the order number of the order.
+     * Every order number is unique.
+     * @return A random generated string with digits and letters.
+     */
+    public static String generateOrderNum() {
+        UUID randomUUID = UUID.randomUUID();
+        String orderNumber = randomUUID.toString().replaceAll("-", "").substring(0, 10);
+        while (totalOrder.containsKey(orderNumber)) {
+            randomUUID = UUID.randomUUID();
+            orderNumber = randomUUID.toString().replaceAll("-", "").substring(0, 10);
+        }
+        return orderNumber;
+    }
 }
