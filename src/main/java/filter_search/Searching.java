2a2e326587648db20fca39466820e5de75e371cd
donnaqi <donna021107@gmail.com>
2022-11-17 21:39:54 -0500

    Slight adjustment

--- src/main/java/filter_search/Searching.java
+++ src/main/java/filter_search/Searching.java
@@ -1,4 +1,4 @@
-package filtersearch;
+package filter_search;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,21 +9,21 @@ import helpers.Deserializer;
 
 public class Searching {
     protected static ArrayList<Drink> drinks = new ArrayList<>();
-    protected static ArrayList filtered_items = new ArrayList<>();
     private static final Deserializer searchDeserializer = new Deserializer();
     public static void constructDrinkDataBase() throws IOException, ClassNotFoundException {
         searchDeserializer.deserialize("./data/drinks");
         drinks = (ArrayList<Drink>) searchDeserializer.getObject();
     }
 
-    public static ArrayList Add_to_Filtered( String keyword){
-        for (int i = 0; i < drinks.size(); i++){
-            String lowercase_name = drinks.get(i).getName().toLowerCase();
-            String lowercase_description = drinks.get(i).getDescription().toLowerCase();
+    public static ArrayList Add_to_Filtered(String keyword){
+        ArrayList filtered_items = new ArrayList<>();
+        for (Drink drink : drinks){
+            String lowercase_name = drink.getName().toLowerCase();
+            String lowercase_description = drink.getDescription().toLowerCase();
             if (lowercase_name.equals(keyword.toLowerCase())){
-                filtered_items.add(drinks.get(i));
+                filtered_items.add(drink);
             } else if (lowercase_description.contains(keyword.toLowerCase())) {
-                filtered_items.add(drinks.get(i));
+                filtered_items.add(drink);
             }
         }
         return filtered_items;
