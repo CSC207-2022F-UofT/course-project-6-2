0731261b4bd89c7403d2ea7b57f743e58b327f7c
Sancia <sancia.gao@mail.utoronto.ca>
2022-11-18 11:20:43 +0800

    Create view class for ShoppingCartScreen and OrderHistoryScreen. Create a ScrollPane class.

---
+++ src/main/java/OrderHistory/View.java
@@ -0,0 +1,7 @@
+package OrderHistory;
+
+public class View {
+    public static void main(String[] args){
+        new OrderHistoryScreen();
+    }
+}
---
+++ src/main/java/ShoppingCart/View.java
@@ -0,0 +1,7 @@
+package ShoppingCart;
+
+public class View {
+    public static void main(String[] args){
+        new ShoppingCartScreen();
+    }
+}
