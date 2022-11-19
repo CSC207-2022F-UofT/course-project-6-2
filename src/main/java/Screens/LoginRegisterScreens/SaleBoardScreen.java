c306553dbc3d727e04e13849e7480291851ecfae
yyuanqinn <yyuanqinn@gmail.com>
2022-11-18 19:40:48 -0500

    Created SaleBoardScreen and ScrollPane

---
+++ src/main/java/Screens/LoginRegisterScreens/SaleBoardScreen.java
@@ -0,0 +1,33 @@
+package Screens.LoginRegisterScreens;
+
+import sale_board.SaleBoard;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SaleBoardScreen extends JFrame implements ActionListener {
+    JFrame frame = new JFrame();
+    ScrollPane scrollPane = new ScrollPane();
+    public SaleBoardScreen() {
+        // Basic setups
+        JPanel panel = new JPanel();
+
+        scrollPane.createScrollPane(frame, panel);
+
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Sale Board");
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
