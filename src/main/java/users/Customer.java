b5f9dfebd702d35e33af7dde738304087d4cb7ff
yyuanqinn <yyuanqinn@gmail.com>
2022-11-18 23:07:19 -0500

    Fixed bug in UserController

--- src/main/java/users/Customer.java
+++ src/main/java/users/Customer.java
@@ -1,57 +1,45 @@
 package users;
 
-import order.Order;
-
-import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Customer extends User implements Serializable {
-    public int age;
-    public ArrayList<Order> orderHistory;
+public class Customer extends User{
+    public int dateOfBirth;
+    public ArrayList<String> orderHistory;
 
     /**
      * Construct an instance of the entity Customer.
      * @param accountName The nickname of the customer.
      * @param passWord The password of the customer.
      * @param phoneNumber The phone number of the customer.
-     * @param age The date of birth of the customer.
+     * @param dateOfBirth The date of birth of the customer.
      * @param address The address of the customer.
      * @param orderHistory The order history of the customer.
      */
-    public Customer(String accountName, String passWord, String phoneNumber, int age, String address, ArrayList<Order> orderHistory){
+    public Customer(String accountName, String phoneNumber, String passWord, int dateOfBirth, String address, ArrayList<String> orderHistory){
         super(accountName, passWord, phoneNumber, address);
-        this.age = age;
+        this.dateOfBirth = dateOfBirth;
         this.orderHistory = orderHistory;
     }
 
     /**
      * Setter methods for the new instance values.
      */
-    public void setAge(int age) {
-        this.age = age;
+    public void setDateOfBirth(int dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 
-    public void setOrderHistory(ArrayList<Order> orderHistory) {
+    public void setOrderHistory(ArrayList<String> orderHistory) {
         this.orderHistory = orderHistory;
     }
 
     /**
      * Getter methods for the new instance values.
      */
-    public int getAge() {
-        return this.age;
+    public int getDateOfBirth() {
+        return this.dateOfBirth;
     }
 
-    public ArrayList<Order> getOrderHistory() {
+    public ArrayList<String> getOrderHistory() {
         return this.orderHistory;
-    }
-
-    public String getOrderStatus(Order order){
-        for(int i = 0; i < this.orderHistory.size(); i++){
-            if (this.orderHistory.get(i).orderNum == order.orderNum){
-                return this.orderHistory.get(i).orderStatus;
-            }
-        }
-        return "Order Not Found";
     }
 }
