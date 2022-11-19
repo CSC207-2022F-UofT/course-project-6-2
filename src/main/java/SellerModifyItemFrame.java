8816961ace66cce9d911c36c56c5d6a42c70dd72
shaoyuq3 <yuqing.shao@mail.utoronto.ca>
2022-11-19 10:57:20 -0500

    New change on java code, done swing form.

--- src/main/java/SellerModifyItemFrame.java
+++ src/main/java/SellerModifyItemFrame.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 
 public class SellerModifyItemFrame extends JFrame{
     private JTextField MFDrinkName;
@@ -20,6 +23,21 @@ public class SellerModifyItemFrame extends JFrame{
         setSize(400, 900);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String StoreName = MFStoreName.getText();
+                String DrinkName = MFDrinkName.getText();
+                float price = Float.parseFloat(MFPrice.getText());
+                String description = MFDescription.getText();
+                String ingredient = MFIngredient.getText();
+                int volume = Integer.parseInt(MFVolume.getText());
+                Date productionData = MFProductionDate.getText();
+                Date expirationDate = MFExpirationDate.getText();
+
+
+            }
+        });
     }
 
     public static void main(String[] args) {
