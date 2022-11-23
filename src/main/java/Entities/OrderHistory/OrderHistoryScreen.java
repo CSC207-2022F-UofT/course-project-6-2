ff490822eacdeaee7180df7a7bd5e7bbbb6d3d94
sanciagao <sancia.gao@mail.utoronto.ca>
2022-11-23 17:58:56 -0500

    save

--- src/main/java/Entities/OrderHistory/OrderHistoryScreen.java
+++ src/main/java/Entities/OrderHistory/OrderHistoryScreen.java
@@ -1,59 +1,107 @@
 package Entities.OrderHistory;
 
-import Screens.OrderHistoryGrouping;
-import Screens.ScrollPane;
+import Screens.ButtonEditor;
+import Screens.ButtonRenderer;
 import UseCases.ExtractUserFromPhoneNum;
-import Screens.Button;
-import Screens.TextField;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.io.IOException;
 
-public class OrderHistoryScreen extends JFrame implements ActionListener {
 
-    static JFrame frame = new JFrame();
-    JPanel panel = new JPanel();
-    TextField orderNumber = new TextField();
-    Button searchButton = new Button();
-    JButton reOrderButton = new JButton();
-    JLabel orderLabels = new JLabel();
-    ScrollPane scrollPane = new ScrollPane();
-    OrderHistoryGrouping orderGroup = new OrderHistoryGrouping();
-    //orderLabel orders = new orderLabel();
-    ExtractUserFromPhoneNum getCustomer = new ExtractUserFromPhoneNum();
+/**
+ *
+ * @author sanciagao
+ */
+public class OrderHistoryScreen extends JFrame {
 
-    public OrderHistoryScreen() {
+    // Variables declaration
+    JButton searchButton = new JButton("Search");
+    JTextField searchOrderHistory = new JTextField();
+    JScrollPane scrollPane = new JScrollPane();
+    JTable table = new JTable();
+    ExtractUserFromPhoneNum extractedUser = new ExtractUserFromPhoneNum();
+    public OrderHistoryScreen(){
 
-        //creating frame
-        frame.setSize(800, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setTitle("Order History");
-        panel.setSize(frame.getSize());
-        scrollPane.createScrollPane(frame, panel);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setSize(new java.awt.Dimension(800, 600));
+        scrollPane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
-        // adding the components onto the panel
-        orderNumber.createTextField(panel,25, 25, 300, 30);
-        searchButton.createButton(panel,"Search", 375,25,75,30);
+        searchOrderHistory.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SearchOrderHistoryActionPerformed(evt);
+            }
+        });
 
-        orderLabels.setText("1");
-        orderLabels.setSize(100, 50);
+        // Creating the table
+        table.setModel(new javax.swing.table.DefaultTableModel(
+                new Object [][] {
+                        {null, null, null, "Re-Order"},
+                        {null, null, null, "Re-Order"},
+                        {null, null, null, "Re-Order"},
+                        {null, null, null, "Re-Order"},
+                        {null, null, null, "Re-Order"},
+                        {null, null, null, "Re-Order"},
+                        {null, null, null, "Re-Order"}
+                }, new String [] {
+                        "Order Name", "Order Price", "Order Status", "Re-Order"
+                }
+        ) {
+            // making the columns uneditable
+            final boolean[] canEdit = new boolean [] {
+                    false, false, false, false
+            };
 
-        orderGroup.grouping(panel, orderLabels, reOrderButton);
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
 
-        //orders.createOrderLabel(panel, order);
+        // Setting the settings for the table
+        scrollPane.setViewportView(table);
+        table.setFillsViewportHeight(true);
+        table.getTableHeader().setReorderingAllowed(false);
 
-        frame.setVisible(true);
+        // adding buttons for the column with "Re-Order" as the column name
+        table.getColumn("Re-Order").setCellRenderer(new ButtonRenderer());
+        table.getColumn("Re-Order").setCellEditor(
+                new ButtonEditor(new JCheckBox()));
+
+        // Setting Columns to be not resizable
+        for (int i = 0; i > table.getColumnCount(); i++){
+            if (table.getColumnModel().getColumnCount() > 0) {
+                table.getColumnModel().getColumn(i).setResizable(false);
+            }
+        }
+
+        // Creating layout of OrderHistory Screen
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(searchOrderHistory, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(26, 26, 26)
+                                .addComponent(searchButton, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(457, Short.MAX_VALUE))
+                        .addComponent(scrollPane, javax.swing.GroupLayout.Alignment.TRAILING)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(searchOrderHistory, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(searchButton, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+                                .addComponent(scrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 510, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        pack();
     }
-    /**
-     * Invoked when action occurs
-     *
-     * @param e the event to be processed
-     */
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    private void SearchOrderHistoryActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
     }
 }
+
