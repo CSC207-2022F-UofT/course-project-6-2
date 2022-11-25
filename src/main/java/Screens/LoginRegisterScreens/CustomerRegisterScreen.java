d3b30a21df4207a14c0960277e60dd842488f10a
yyuan-qin <113566884+yyuan-qin@users.noreply.github.com>
2022-11-24 19:39:59 -0500

    Created RegisterInputBoundary

--- src/main/java/Screens/LoginRegisterScreens/CustomerRegisterScreen.java
+++ src/main/java/Screens/LoginRegisterScreens/CustomerRegisterScreen.java
@@ -1,6 +1,7 @@
 package Screens.LoginRegisterScreens;
 
 import Screens.*;
+import UseCases.UserInputBoundary.RegisterInputBoundary;
 import UseCases.UserUseCases.RegisterUser;
 
 import javax.swing.*;
@@ -94,14 +95,8 @@ public class CustomerRegisterScreen extends JFrame implements ActionListener {
         String confirmPass = String.valueOf(confirmPassField.getPassword());
         String address = addressField.getText();
         String ageText = ageField.getText();
+        int age = new RegisterInputBoundary(ageText).age;
 
-        int age = 0;
-        try {
-            Double.parseDouble(ageText);
-            age = Integer.parseInt(ageText);
-        } catch(Exception ignored){
-
-        }
         if(e.getSource() == registerButton.button){
             boolean registerResult = RegisterUser.registerUser(accountName, phoneNum, password, confirmPass, address, age, null);
             if (registerResult) {
