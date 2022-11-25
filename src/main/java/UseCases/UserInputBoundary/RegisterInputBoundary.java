497d00c7970962c03f4a645d2af2464fbca1f258
yyuan-qin <113566884+yyuan-qin@users.noreply.github.com>
2022-11-24 19:39:59 -0500

    Created RegisterInputBoundary

---
+++ src/main/java/UseCases/UserInputBoundary/RegisterInputBoundary.java
@@ -0,0 +1,11 @@
+package UseCases.UserInputBoundary;
+
+public class RegisterInputBoundary {
+    public int age = 0;
+    public RegisterInputBoundary(String ageText) {
+        try {
+            Double.parseDouble(ageText);
+            this.age = Integer.parseInt(ageText);
+        } catch(Exception ignored){}
+    }
+}
