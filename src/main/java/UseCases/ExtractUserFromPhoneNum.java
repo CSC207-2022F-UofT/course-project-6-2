b1af3a58693fb3f0acdfa45dab9804ad7f81e268
donnaqi <donna021107@gmail.com>
2022-11-26 01:57:49 -0500

    Fix bug in ExtractUser

--- src/main/java/UseCases/ExtractUserFromPhoneNum.java
+++ src/main/java/UseCases/ExtractUserFromPhoneNum.java
@@ -1,29 +1,19 @@
 package UseCases;
 
 import Entities.Users.User;
+import UseCases.UserUseCases.CreateUserHashMap;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
+
+
 
 public class ExtractUserFromPhoneNum{
 
-    Helpers.Deserializer userDeserializer = new Helpers.Deserializer();
-    HashMap<String, User> sellers = new HashMap<>();
-    HashMap<String, User> customers = new HashMap<String, User>();
-
     public User extractUser(String phoneNum) throws IOException, ClassNotFoundException {
-        userDeserializer.deserialize("./data/users");
-        ArrayList<HashMap> data = (ArrayList<HashMap>) userDeserializer.getObject();
-        if (data != null) {
-            sellers = data.get(0);
-            customers = data.get(1);
-        }
-
-        if (sellers.get(phoneNum).getPhoneNumber().equals(phoneNum)) {
-            return sellers.get(phoneNum);
+        if (CreateUserHashMap.getSellers().get(phoneNum) != null) {
+            return CreateUserHashMap.getSellers().get(phoneNum);
         } else {
-            return customers.get(phoneNum);
+            return CreateUserHashMap.getCustomers().get(phoneNum);
         }
     }
 }
--- src/main/java/UseCases/UserUseCases/CreateUserHashMap.java
+++ src/main/java/UseCases/UserUseCases/CreateUserHashMap.java
@@ -9,4 +9,12 @@ import java.util.HashMap;
 public class CreateUserHashMap {
     protected static HashMap<String, User> sellers = new HashMap<>();
     protected static HashMap<String, User> customers = new HashMap<>();
+
+    public static HashMap<String, User> getSellers() {
+        return sellers;
+    }
+
+    public static HashMap<String, User> getCustomers() {
+        return customers;
+    }
 }
