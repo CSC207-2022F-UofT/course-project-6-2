c274a436a5eeacd1c8a87e60c9aa0c285bb2945a
donnaqi <donna021107@gmail.com>
2022-11-26 22:33:17 -0500

    Fix bug in SaleBoard

--- src/main/java/Screens/LoginRegisterScreens/SaleBoardScreen.java
+++ src/main/java/Screens/LoginRegisterScreens/SaleBoardScreen.java
@@ -1,7 +1,5 @@
 package Screens.LoginRegisterScreens;
 
-import sale_board.SaleBoard;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- src/main/java/UseCases/GetOnSaleDrinks.java
+++ src/main/java/UseCases/GetOnSaleDrinks.java
@@ -22,5 +22,4 @@ public class GetOnSaleDrinks {
         }
         return onSaleDrinks;
     }
-
 }
