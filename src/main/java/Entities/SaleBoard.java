b6332d07dd262032de01934672ef5f66fa7072ce
lisacheng12 <jiayilisa27@gmail.com>
2022-11-27 18:05:59 -0500

    fix the saleBoard error

--- src/main/java/Entities/SaleBoard.java
+++ src/main/java/Entities/SaleBoard.java
@@ -1,17 +1,20 @@
 package Entities;
 
-import UseCases.DrinkRuntimeDataBase;
+import UseCases.DrinkUseCases.DrinkRuntimeDataBase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class SaleBoard {
 
-    public ArrayList<Drink> onSale(){
+    public ArrayList<Drink> onSale() {
         ArrayList onSaleDrinks = new ArrayList<>();
-        for (Map.Entry<String, Drink> drink : DrinkRuntimeDataBase.drinks.entrySet()){
-            if (drink.getValue().getDiscount() != 1) {
-                onSaleDrinks.add(drink);
+        for (Map.Entry<String, HashMap<String, Drink>> drinkSet : DrinkRuntimeDataBase.getDrinks().entrySet()) {
+            for (Map.Entry<String, Drink> drink : drinkSet.getValue().entrySet()) {
+                if (drink.getValue().getDiscount() != 1) {
+                    onSaleDrinks.add(drink);
+                }
             }
         }
         return onSaleDrinks;
