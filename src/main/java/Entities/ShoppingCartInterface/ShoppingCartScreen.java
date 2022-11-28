bfcde3b2062dd30bcfef2387a9fcb8835c553d8f
sanciag17 <sancia.gao@mail.utoronto.ca>
2022-11-27 20:36:10 -0500

    Create GetSumOfDrinks Use case and ShoppingCartPanel screen.

--- src/main/java/Entities/ShoppingCartInterface/ShoppingCartScreen.java
+++ src/main/java/Entities/ShoppingCartInterface/ShoppingCartScreen.java
@@ -1,5 +1,8 @@
 package Entities.ShoppingCartInterface;
 
+import Screens.ShoppingCartPanel;
+import UseCases.GetSumOfDrinks;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,16 +20,18 @@ public class ShoppingCartScreen extends JFrame {
     JButton addQuantity = new JButton("+");
     JButton minusQuantity = new JButton("-");
     JScrollPane innerScrollPane = new JScrollPane();
+    ShoppingCartPanel shoppingCartPanel = new ShoppingCartPanel();
     JTable table = new JTable();
     JPanel panel = new JPanel();
     JLabel totalAmountLabel = new JLabel();
     JLabel totalLabel = new JLabel("Total: ");
     ArrayList<Double> totalAmount = new ArrayList<>();
     Double total = 0.0;
-    private static final DecimalFormat df = new DecimalFormat("0.00");
+    DecimalFormat df = new DecimalFormat("0.00");
     Double discountedVal = 0.0;
     Double newVal = 0.0;
-    Double quantity;
+    Double quantity = 1.0;
+    GetSumOfDrinks getSum = new GetSumOfDrinks();
 
     public ShoppingCartScreen() {
 
@@ -36,7 +41,6 @@ public class ShoppingCartScreen extends JFrame {
 
         // scrollPane settings
         innerScrollPane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
 
         // Creating the Table
         table.setModel(new javax.swing.table.DefaultTableModel(
@@ -63,27 +67,19 @@ public class ShoppingCartScreen extends JFrame {
         table.getTableHeader().setReorderingAllowed(false);
 
         // Getting sum of the drink prices
-        for (int i = 0; i < table.getRowCount(); i++) {
-            discountedVal = ((Double)table.getValueAt(i, 1) * (Double)table.getValueAt(i, 2));
-            table.setValueAt((discountedVal*100)/100, i, 4);
-
-            newVal = ((Double)table.getValueAt(i, 3) * (Double)table.getValueAt(i, 4));
-            table.setValueAt((newVal*100)/100, i, 5);
-
-            totalAmount.add((Double)table.getValueAt(i, 5));
-            total += totalAmount.get(i);
-        }
+        total = getSum.getSumOfDrinks(table, discountedVal, newVal, totalAmount, total);
 
         // showing the sum
         totalAmountLabel.setText(df.format(total));
 
-
         // Setting Columns to be not resizable
-        for (int i = 0; i > table.getColumnCount(); i++){
+        for (int i = 0; i < table.getColumnCount(); i++){
             if (table.getColumnModel().getColumnCount() > 0) {
                 table.getColumnModel().getColumn(i).setResizable(false);
             }
         }
+
+        // Action Listeners
         checkoutButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
                 checkoutButtonActionPerformed(evt);
@@ -105,42 +101,10 @@ public class ShoppingCartScreen extends JFrame {
 
         javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
         panel.setLayout(panelLayout);
-        panelLayout.setHorizontalGroup(
-                panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(innerScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 798, Short.MAX_VALUE)
-                        .addGroup(panelLayout.createSequentialGroup()
-                                .addGap(27, 27, 27)
-                                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(panelLayout.createSequentialGroup()
-                                                .addComponent(addQuantity)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                .addComponent(checkoutButton)
-                                                .addGap(35, 35, 35))
-                                        .addGroup(panelLayout.createSequentialGroup()
-                                                .addComponent(minusQuantity)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                .addComponent(totalLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                                .addComponent(totalAmountLabel)
-                                                .addGap(50, 50, 50))))
-        );
-        panelLayout.setVerticalGroup(
-                panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(panelLayout.createSequentialGroup()
-                                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(panelLayout.createSequentialGroup()
-                                                .addComponent(innerScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 478, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addGap(18, 18, 18)
-                                                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                                        .addComponent(totalLabel)
-                                                        .addComponent(totalAmountLabel)))
-                                        .addComponent(minusQuantity, javax.swing.GroupLayout.Alignment.TRAILING))
-                                .addGap(18, 18, 18)
-                                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(checkoutButton)
-                                        .addComponent(addQuantity))
-                                .addContainerGap(97, Short.MAX_VALUE))
-        );
+
+        shoppingCartPanel.createShoppingCartPanel(panelLayout, innerScrollPane, addQuantity, checkoutButton, minusQuantity,
+                totalLabel, totalAmountLabel);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -208,8 +172,9 @@ public class ShoppingCartScreen extends JFrame {
 
         if (e.getSource() == minusQuantity) {
             quantity = (Double) table.getValueAt(row, column);
-            if (quantity >= 1) {
+            if (quantity > 0) {
                 table.setValueAt(quantity -= 1.0, row, 3);
+                total -= totalAmount.get(row);
             } else{
                 JOptionPane.showMessageDialog(null,
                         "Sorry, You can not delete anymore drinks");
@@ -222,7 +187,6 @@ public class ShoppingCartScreen extends JFrame {
             newVal = quantity * (Double) table.getValueAt(row, 4);
             table.setValueAt(df.format(newVal), row, 5);
         }
-        total -= totalAmount.get(row);
 
         // showing the sum
         totalAmountLabel.setText(df.format(total));
