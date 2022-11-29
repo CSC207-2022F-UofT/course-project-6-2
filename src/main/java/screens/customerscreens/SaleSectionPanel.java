f3a18db8394089510d39098cf88ac4686b6c1de8
yyuan-qin <113566884+yyuan-qin@users.noreply.github.com>
2022-11-28 23:10:24 -0500

    fixed bugs in SaleSectionPanel

--- src/main/java/screens/customerscreens/SaleSectionPanel.java
+++ src/main/java/screens/customerscreens/SaleSectionPanel.java
@@ -1,9 +1,9 @@
 package screens.customerscreens;
 
-import Entities.Drink;
-import screens.Button;
-import UseCases.DrinkUseCases.GetOnSaleDrinks;
-import UseCases.UserUseCases.UserRuntimeDataBase;
+import entities.Drink;
+import screens.swingcomponents.Button;
+import usecases.drinkusecases.GetOnSaleDrinks;
+import usecases.userusercases.UserRuntimeDataBase;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -27,8 +27,8 @@ public class SaleSectionPanel extends JFrame implements ActionListener {
 
         Vector<String> headers = new Vector<>();
         Vector<Vector<String>> data = new Vector<>();
-        Vector<String> line = new Vector<>();
-        ArrayList<Drink> onSaleDrinks = new GetOnSaleDrinks().onSale();
+        //ArrayList<Drink> onSaleDrinks = new GetOnSaleDrinks().onSale();
+        ArrayList<Drink> onSaleDrinks = new DrinkTester().drinkArrayList();
         DecimalFormat df = new DecimalFormat("0.00");
 
         headers.add("Drink name");
@@ -37,21 +37,12 @@ public class SaleSectionPanel extends JFrame implements ActionListener {
         headers.add("Current Price");
 
         for (Drink drink: onSaleDrinks) {
+            Vector<String> line = new Vector<>();
             line.add(drink.getName());
             line.add("$" + drink.getPrice());
-            line.add((1 - drink.getDiscount()) * 100 + "%");
+            line.add(df.format((1 - drink.getDiscount()) * 100) + "%");
             line.add("$" + df.format(drink.getPrice() * drink.getDiscount()));
             data.add(line);
-        }
-
-        // test part
-        for (int i = 0; i < 30; i ++) {
-            Vector<String> line1 = new Vector<>();
-            line1.add("Strawberry Coconut");
-            line1.add("$18.00");
-            line1.add("30%");
-            line1.add("$12.60");
-            data.add(line1);
         }
 
         drinkTable = new JTable(data, headers);
