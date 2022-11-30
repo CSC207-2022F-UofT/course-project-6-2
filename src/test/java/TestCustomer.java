c5c2c3b28b288919c856dbd8f570f4db7b05418c
donnaqi <donna021107@gmail.com>
2022-11-29 20:18:55 -0500

    Re-format the test structure

--- src/test/java/TestCustomer.java
+++ src/test/java/TestCustomer.java
@@ -1,6 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import entities.users.Customer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,15 +10,36 @@ import org.junit.jupiter.api.Test;
  * further properties like getShoppingCart will be tested in UsecaseTest.
  */
 public class TestCustomer {
+    Customer customer1 = new Customer("Amy", "7780000000",
+            "abcd", 20, "College St.");
+
     @Test
-    @DisplayName("Customer test")
-    void testCustomer(){
-        Customer customer1 = new Customer("Amy", "7780000000",
-                "abcd", 20, "College St.");
+    @DisplayName("Get age test")
+    void getAgeTest(){
         assertEquals(customer1.getAge(), 20);
-        assertEquals(customer1.getPassWord(), "abcd");
+    }
+
+    @Test
+    @DisplayName("Get phone number test")
+    void getPhoneNumTest(){
         assertEquals(customer1.getPhoneNumber(), "7780000000");
+    }
+
+    @Test
+    @DisplayName("Get age test")
+    void getPasswordTest() {
+        assertEquals(customer1.getPassWord(), "abcd");
+    }
+
+    @Test
+    @DisplayName("Get account number test")
+    void getAccountNameTest(){
         assertEquals(customer1.getAccountName(), "Amy");
+    }
+
+    @Test
+    @DisplayName("Get address test")
+    void name() {
         assertEquals(customer1.getAddress(), "College St.");
     }
 }
