56c5df6ed8050feed965a9f7cd94c23dc355d054
yyuan-qin <113566884+yyuan-qin@users.noreply.github.com>
2022-12-01 00:32:43 -0500

    fixed minor bug

--- src/main/java/screens/customerscreens/CustomerMainScreen.java
+++ src/main/java/screens/customerscreens/CustomerMainScreen.java
@@ -1,8 +1,8 @@
 package screens.customerscreens;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import java.util.Objects;
 
 /**
  * The customer main screen containing the drinks, search drinks, sale board,
