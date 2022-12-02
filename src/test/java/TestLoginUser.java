9a566db6246211cf732e5c111b49ceb040f89079
AdwinTazement <xiyuan.jin@mail.utoronto.ca>
2022-12-02 12:34:49 +0800

    fixed some bugs in TestLoginUser

--- src/test/java/TestLoginUser.java
+++ src/test/java/TestLoginUser.java
@@ -1,7 +1,37 @@
+import entities.Drink;
+import entities.Order;
+import entities.users.Customer;
+import entities.users.Seller;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import usecases.customerusecases.AddToOrderHistory;
+import usecases.databaseusecases.UserRuntimeDataBase;
 import usecases.loginregisterusecases.LoginUser;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestLoginUser {
+    @Test
+    @BeforeEach
+    public void testLoginUserSetup() {
+        Customer customer1 = new Customer("Sara", "1234", "abcd", 18, "Bay");
+        Seller seller1 = new Seller("Aras", "4321", "dcba", "Bay", "Super Pig");
+        UserRuntimeDataBase.getCustomers().put(customer1.getPhoneNumber(), customer1);
+        UserRuntimeDataBase.getSellers().put(seller1.getPhoneNumber(), seller1);
+        UserRuntimeDataBase.constructCurrentCustomer("1234");
+        UserRuntimeDataBase.constructCurrentSeller("4321");
+    }
+    @Test
+    public void testLoginUser(){
+        /*test customer login*/
+        assertEquals(LoginUser.login("1234", "abcd"), "Customer");
+        /*test seller login*/
+        assertEquals(LoginUser.login("4321", "123"), "Seller");
+        /*test other login*/
+        assertEquals(LoginUser.login("9327486349", "123"), "No user");
+    }
 }
