6f18478416229e34dfddbd39f4ba6cb45f6e5225
AdwinTazement <xiyuan.jin@mail.utoronto.ca>
2022-12-05 13:29:08 +0800

    fixed some bugs

---
+++ src/test/java/TestSearchDrinks.java
@@ -0,0 +1,17 @@
+import entities.Drink;
+import entities.Order;
+import entities.users.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import usecases.customerusecases.AddToOrderHistory;
+import usecases.databaseusecases.UserRuntimeDataBase;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+public class TestSearchDrinks {
+
+    @Test
+
+}
