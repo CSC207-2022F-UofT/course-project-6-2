5c2781e26d67114c26426905c4884236ef55c938
AdwinTazement <xiyuan.jin@mail.utoronto.ca>
2022-12-05 13:29:08 +0800

    fixed some bugs

--- src/main/java/screens/customerscreens/WarningBoard.java
+++ src/main/java/screens/customerscreens/WarningBoard.java
@@ -6,55 +6,40 @@ import usecases.databaseusecases.UserRuntimeDataBase;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class WarningBoard extends JPanel {
-
-    // defining variables
-    private final JLabel warning = new JLabel();
-    private final GetWarningMessage warningMessage = new GetWarningMessage();
-    private final ShoppingCart shoppingCart = UserRuntimeDataBase.getCurrentCustomer().getShoppingCart();
-
     public WarningBoard() {
 
         // defining local variables
         JFrame jf = new JFrame();
         JPanel top = new JPanel();
         JPanel bottom = new JPanel();
-        JButton showWarning = new JButton("Show Warning");
         JButton closeButton = new JButton("I Understand");
 
         //setting up frame
         jf.setTitle("Warning Board");
         jf.setBounds(460, 300, 500, 120);
         jf.setResizable(false);
-        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         jf.setVisible(true);
 
         // set up panels
+        // defining variables
+        JLabel warning = new JLabel();
         top.add(warning);
         warning.setOpaque(true);
-        bottom.add(showWarning);
         bottom.add(closeButton);
 
         //set up contentpane
         jf.add(bottom, BorderLayout.SOUTH);
         jf.add(top, BorderLayout.NORTH);
 
-        showWarning.addActionListener(new ActionListener() {
-            @Override
-            /*Change JLabel to "Drinks in shopping cart contains what".*/
-            public void actionPerformed(ActionEvent e) {
-                warning.setText(warningMessage.getWarningMessage(shoppingCart.getItemList()));
-            }
-        });
-        closeButton.addActionListener(new ActionListener() {
-            @Override
-            /*exit.*/
-            public void actionPerformed(ActionEvent e) {
-                jf.dispose();
-            }
-        });
+        /*Change JLabel to "Drinks in shopping cart contains what".*/
+        GetWarningMessage warningMessage = new GetWarningMessage();
+        ShoppingCart shoppingCart = UserRuntimeDataBase.getCurrentCustomer().getShoppingCart();
+        warning.setText(warningMessage.getWarningMessage(shoppingCart.getItemList()));
+
+        /*exit.*/
+        closeButton.addActionListener(e -> jf.dispose());
     }
 }
