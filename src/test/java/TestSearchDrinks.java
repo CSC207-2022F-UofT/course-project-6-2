9960dcef333557692b55c07f2fd56de97b4d7fe4
AdwinTazement <xiyuan.jin@mail.utoronto.ca>
2022-12-06 08:19:23 +0800

    fixed some bugs

--- src/test/java/TestSearchDrinks.java
+++ src/test/java/TestSearchDrinks.java
@@ -20,19 +20,17 @@ public class TestSearchDrinks {
     @Test
     @BeforeEach
     public void testOrderHistorySetup() {
-        String s1 = "s1";
-        String s2 = "s2";
-        String s3 = "s3";
-        String s4 = "s4";
+        DrinkRuntimeDataBase.addStore("store1");
+        DrinkRuntimeDataBase.addStore("store2");
         Drink drink1 = new Drink("testName1", 5.4f, "testDiscription1", "apple", 100, new Date(), new Date(), 0.8f);
         Drink drink2 = new Drink("testName2", 2.0f, "testDiscription2", "milk", 1, new Date(), new Date(), 1f);
-        DrinkRuntimeDataBase.setDrinks(drinks);
         HashMap<String, Drink> list1 = new HashMap<>();
         HashMap<String, Drink> list2 = new HashMap<>();
-        list1.put(s1, drink1);
-        list2.put(s2, drink2);
-        drinks.put(s3, list1);
-        drinks.put(s4, list2);
+        list1.put("testName1", drink1);
+        list2.put("testName2", drink2);
+        drinks.put("store1", list1);
+        drinks.put("store2", list2);
+        DrinkRuntimeDataBase.setDrinks(drinks);
     }
     @Test
     public void testSearchDrinks() {
