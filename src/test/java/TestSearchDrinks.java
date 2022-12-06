429fdee80cdc3674de7fede7504a17dba0895442
AdwinTazement <xiyuan.jin@mail.utoronto.ca>
2022-12-06 08:11:16 +0800

    fixed some bugs

--- src/test/java/TestSearchDrinks.java
+++ src/test/java/TestSearchDrinks.java
@@ -16,13 +16,23 @@ import java.util.Vector;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestSearchDrinks {
+    private HashMap<String, HashMap<String, Drink>> drinks;
     @Test
     @BeforeEach
     public void testOrderHistorySetup() {
+        String s1 = "s1";
+        String s2 = "s2";
+        String s3 = "s3";
+        String s4 = "s4";
         Drink drink1 = new Drink("testName1", 5.4f, "testDiscription1", "apple", 100, new Date(), new Date(), 0.8f);
         Drink drink2 = new Drink("testName2", 2.0f, "testDiscription2", "milk", 1, new Date(), new Date(), 1f);
-        DrinkRuntimeDataBase.getDrinkList().add(drink1);
-        DrinkRuntimeDataBase.getDrinkList().add(drink2);
+        DrinkRuntimeDataBase.setDrinks(drinks);
+        HashMap<String, Drink> list1 = new HashMap<>();
+        HashMap<String, Drink> list2 = new HashMap<>();
+        list1.put(s1, drink1);
+        list2.put(s2, drink2);
+        drinks.put(s3, list1);
+        drinks.put(s4, list2);
     }
     @Test
     public void testSearchDrinks() {
