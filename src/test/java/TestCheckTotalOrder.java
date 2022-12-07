e93ac6e6ef897b1ebbb98964faf1f01e368b408d
TingmingFan <tingming.fan@mail.utoronto.ca>
2022-12-07 16:35:01 -0500

    autographing update

--- src/test/java/TestCheckTotalOrder.java
+++ src/test/java/TestCheckTotalOrder.java
@@ -32,6 +32,16 @@ public class TestCheckTotalOrder {
     @Test
     @DisplayName("Test Total order")
     public void testTotalOrder(){
+        Customer customer1 = new Customer("Sara", "6041231234", "abcd", 18, "Bay");
+        UserRuntimeDataBase.getCustomers().put(customer1.getPhoneNumber(), customer1);
+        UserRuntimeDataBase.constructCurrentCustomer("6041231234");
+        Drink drink1 = new Drink("apple", 5.4f, "good", "apple", 100, new Date(), new Date(), 0.8f);
+        Drink drink2 = new Drink("milk", 2.0f, "great", "milk", 1, new Date(), new Date(), 1f);
+        HashMap<Drink, Integer> orderList = new HashMap<>();
+        HashMap<Drink, Integer> orderList2 = new HashMap<>();
+        orderList.put(drink1, 2);
+        float totalPrice = drink1.getPrice() * 2 + drink2.getPrice() * 5;
+        AddToOrderHistory.addToOrderHistory(orderList, totalPrice);
         assertEquals(UserRuntimeDataBase.getCurrentCustomer().getOrderHistory().get(0).getTotalOrder(),
-                1);}
+                2);}
 }
